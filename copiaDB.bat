572428e234f8463eabb81e005f97ddf1fdf913b0
Luciano <luciano.calderano@mobilesoft.it>
2019-02-15 10:29:16 +0100

    cir

---
+++ copiaDB.bat
@@ -0,0 +1,6 @@
+﻿@echo off
+@for /F "tokens=1-4 delims=/ " %%i in ('date /t') do set WD=%%i
+
+C:\wamp\bin\mysql\mysql5.6.17\bin\mysqldump -u root cir > y:\copiaDatabase.%WD%.sql
+C:\wamp\bin\mysql\mysql5.6.17\bin\mysqldump -u root cir > z:\copiaDatabase.sql
+C:\wamp\bin\mysql\mysql5.6.17\bin\mysqldump -u root cir > c:\copiaDatabase.sql
